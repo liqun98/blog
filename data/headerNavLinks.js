c19d9da0f3590e0de8cbcfaeaee17a4c30dab100
CodeMaker-Zhao <zlq1440169549@qq.com>
2023-01-09 12:20:17 +0800

    add weekly

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,9 +1,9 @@
 const headerNavLinks = [
   { href: '/blog', title: '博客' },
   { href: '/tags', title: '标签' },
-  { href: '/projects', title: '项目' },
-  { href: '/about', title: '关于' },
   { href: '/timeline', title: '时间线' },
+  { href: '/weekly', title: '周报' },
+  { href: '/about', title: '关于' },
 ]
 
 export default headerNavLinks
--- pages/weekly/page/[page].js
+++ pages/weekly/page/[page].js
@@ -2,11 +2,11 @@ import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import ListLayout from '@/layouts/ListLayout'
-import { POSTS_PER_PAGE } from '../../blog'
+import { WEEKLYS_PER_PAGE } from '../../weekly'
 
 export async function getStaticPaths() {
-  const totalPosts = await getAllFilesFrontMatter('blog')
-  const totalPages = Math.ceil(totalPosts.length / POSTS_PER_PAGE)
+  const totalPosts = await getAllFilesFrontMatter('weekly')
+  const totalPages = Math.ceil(totalPosts.length / WEEKLYS_PER_PAGE)
   const paths = Array.from({ length: totalPages }, (_, i) => ({
     params: { page: (i + 1).toString() },
   }))
@@ -21,15 +21,15 @@ export async function getStaticProps(context) {
   const {
     params: { page },
   } = context
-  const posts = await getAllFilesFrontMatter('blog')
+  const posts = await getAllFilesFrontMatter('weekly')
   const pageNumber = parseInt(page)
   const initialDisplayPosts = posts.slice(
-    POSTS_PER_PAGE * (pageNumber - 1),
-    POSTS_PER_PAGE * pageNumber
+    WEEKLYS_PER_PAGE * (pageNumber - 1),
+    WEEKLYS_PER_PAGE * pageNumber
   )
   const pagination = {
     currentPage: pageNumber,
-    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+    totalPages: Math.ceil(posts.length / WEEKLYS_PER_PAGE),
   }
 
   return {
@@ -49,8 +49,8 @@ export default function PostPage({ posts, initialDisplayPosts, pagination }) {
         posts={posts}
         initialDisplayPosts={initialDisplayPosts}
         pagination={pagination}
-        title="All Posts"
-        folder="blog"
+        title="All Weekly"
+        folder="weekly"
       />
     </>
   )
