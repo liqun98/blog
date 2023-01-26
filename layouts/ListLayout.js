8398f828283ebaf1f4c67d3f3b3e429926fcf684
CodeMaker-Zhao <zlq1440169549@qq.com>
2023-01-26 20:00:03 +0800

    修复tag页跳转bug

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -50,7 +50,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
         <ul>
           {!filteredPosts.length && 'No posts found.'}
           {displayPosts.map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
+            const { slug, date, title, summary, tags, folder } = frontMatter
             return (
               <li key={slug} className="py-4">
                 <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
